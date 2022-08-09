640396d5c7f212a14faeaac2cf0bcb1636894416
keven william silva barros <69265078+kevenbarros@users.noreply.github.com>
2022-08-09 20:33:29 -0300

    feat(app):mouse customization
    mouse customization

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
+import { useEffect } from "react";
 import Nav from "./pages/home/Nav";
 import styles from "./styles/Home.module.css"
 function App() {
+  useEffect(() => {
+    mouseEdit()
+  }, [])
+  function mouseOverNav() {
+    const cursorRounded = document.getElementById('cursorBox');
+    const cursorBoxinternal = document.getElementById('cursorBoxinternal')
+    if (cursorRounded) {
+      cursorRounded.style.borderColor = '#ffff';
+      cursorBoxinternal.style.backgroundColor = '#ffff';
+    }
+  }
+  function mouseOutNav() {
+    console.log('saiu')
+    const cursorRounded = document.getElementById('cursorBox');
+    const cursorBoxinternal = document.getElementById('cursorBoxinternal')
+    if (cursorRounded) {
+      cursorRounded.style.borderColor = '#061A30';
+      cursorBoxinternal.style.backgroundColor = '#061A30';
+    }
+  }
+  function mouseEdit() {
+    const cursorRounded = document.getElementById('cursorBox');
+
+    const moveCursor = (e) => {
+      let mouseY = e.clientY - 15;
+      let mouseX = e.clientX - 15;
+      if (cursorRounded) {
+        cursorRounded.style.top = `${mouseY}px`
+        cursorRounded.style.left = `${mouseX}px`
+      }
+    }
+    window.addEventListener('mousemove', moveCursor)
+  }
 
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.mainNavigation}>
-
+      <div className={styles.mainNavigation} onMouseOver={mouseOverNav} id="navBox">
       </div>
-      <div className={styles.dalhe}>
-
+      <div className={styles.dalhe} onMouseOver={mouseOutNav}>
         <Nav />
       </div>
-    </div>
+      <div className={styles.cursorBox} id="cursorBox">
+        <div className={styles.cursorBoxinternal} id="cursorBoxinternal">
+        </div>
+      </div>
+    </div >
   );
 }
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/home/skills.module.scss'
+
+
+export default function Button() {
+
+  return (
+    <div className={styles.skillsBox} >
+
+      <div className={styles.bomDia}> <MouseParallax><h1>Skills</h1></MouseParallax></div>
+    </div>
+  )
+}
