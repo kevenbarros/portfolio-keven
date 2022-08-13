7d6d96fa885bebbd0e62e143c4bb2995861bf097
keven william silva barros <69265078+kevenbarros@users.noreply.github.com>
2022-08-13 11:52:17 -0300

    contact commit (1)

--- src/pages/home/Contact.jsx
+++ src/pages/home/Contact.jsx
@@ -1,14 +1,34 @@
 import styles from '../../styles/home/contact.module.scss'
 import { MouseParallax, ScrollParallax } from "react-just-parallax";
 import Button from '../../components/Button';
+import coffe from '../../images/coffe.svg'
 import { useTranslation } from 'react-i18next';
 export default function Contact() {
   const { t } = useTranslation();
   return (
     <div className={styles.contactBox} >
-
-      <div className={styles.bomDia}>
-        <Button /> <MouseParallax><h1>{t("teste1")}</h1></MouseParallax>
+      <div className={styles.contactContent}>
+        <form action="https://api.staticforms.xyz/submit" method="POST" className={styles.formBox}>
+          <input type="hidden" name="accessKey" value="a652af61-d092-4dce-85ff-b90bf4d0af65" />
+          <input type="hidden" name="redirectTo" value="https://portfolio-keven.vercel.app/" />
+          <h3>{t('contact.titleForm')}</h3>
+          <div>
+            <label htmlFor="">{t("contact.lableName")}</label>
+            <input type="text" name="name" placeholder={t("contact.placeholder.name")} />
+          </div>
+          <div>
+            <label htmlFor="">{t("contact.labeleEmail")}</label>
+            <input type="text" name="email" placeholder={t("contact.placeholder.email")} />
+          </div>
+          <div>
+            <label htmlFor="">{t("contact.lableMessage")}</label>
+            <textarea name="message" id="" maxlength="30" cols="30" rows="8" placeholder={t("contact.placeholder.description")}></textarea>
+          </div>
+          <button className={styles.button} >{t('contact.button')}</button>
+        </form>
+        <div>
+          <img src={coffe} alt="" className={styles.coffe} />
+        </div>
       </div>
     </div>
   )
--- src/pages/home/Experiences.jsx
+++ src/pages/home/Experiences.jsx
@@ -1,12 +1,35 @@
 import styles from '../../styles/home/experiences.module.scss'
 import { MouseParallax, ScrollParallax } from "react-just-parallax";
+import { useTranslation } from 'react-i18next';
 
 export default function Experiences() {
+  const { t } = useTranslation();
 
   return (
     <div className={styles.experiencesBox} >
+      <div className={styles.contactBox} >
+        <div className={styles.contactContent}>
+          <form action="" className={styles.formBox}>
+            <h3>{t('contact.titleForm')}</h3>
+            <div>
+              <label htmlFor=""></label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor=""></label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor=""></label>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
 
-      <div className={styles.bomDia}> <MouseParallax><h1>Experiences</h1></MouseParallax></div>
+          </form>
+          <div>
+            <img src='' alt="" className={styles.coffe} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
